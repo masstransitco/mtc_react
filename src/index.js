34639a4729532be66652ca03a48e056d1492dde3
Neuter <mark@air.city>
2024-11-26 18:58:38 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import { AppProvider } from './context/AppContext';
 import './styles/globals.css';
 import ErrorBoundary from './ErrorBoundary';
-
+ 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>

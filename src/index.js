9dad59edb7bb5c715740e46df9081118d015aa09
Neuter <mark@air.city>
2024-11-26 19:02:03 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AppProvider } from './context/AppContext';
 import './styles/globals.css';
-import ErrorBoundary from './ErrorBoundary';
+import ErrorBoundary from './components/ErrorBoundary';
  
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
